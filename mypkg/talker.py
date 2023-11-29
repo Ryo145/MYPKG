75469039b8fd0fbfd2243c6d78515149b42c1ca6
Ryosuke <ryo135791113@gmail.com>
2023-11-30 08:49:27 +0900

    init

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,18 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "鈴木涼介":
+        response.age = 19
+    else:
+        response.age = 255
+
+    return response
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person, "person", 10) #変更
-n = 0
-def cb():
-    global n
-    msg = Person()
-    msg.name = "鈴木涼介"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
-
-node.create_timer(0.5, cb)
+pub = node.create_service(Query, "query", cb)
 rclpy.spin(node) 
